b0a9726ee56c8dae88a606055dff2bf6d860b53b
Zadie Moon <zadiemoon.println@gmail.com>
2023-08-17 14:05:17 -0700

    node server debug

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import "dotenv/config";        //import dotenv library to read .env file
 import mongoose from "mongoose"; //import mongoose library to connect to MongoDB
 
 //connect to MongoDB
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+const CONNECTION_STRING = "mongodb+srv://user:suburban@cluster60017.rns8tih.mongodb.net/tuiter?retryWrites=true&w=majority";
 // || 'mongodb://127.0.0.1:27017/tuiter'; 
 mongoose.connect(CONNECTION_STRING);
 
